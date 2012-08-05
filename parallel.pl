49591a247a7e60f3042c36757e63550d8727139a
Jeremie Le Hen <jeremie@le-hen.org>
2012-08-05 19:22:00 +0200

    Add the BSD license on top of each file.

--- parallel.pl
+++ parallel.pl
@@ -5,6 +5,24 @@
 # Based on multremsh.pl:
 # 2004 Vincent Haverlant <vincent.haverlant@sgcib.com> 
 # $Id: multremsh.pl 73 2007-11-12 13:12:23Z vhaverla $
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 use strict;
 use warnings;
@@ -211,7 +229,6 @@ $dirname = $0;
 $dirname =~ s{\/?[^/]+$}{};
 if ($dirname eq '') { $dirname = '.' }
 unshift @INC, $dirname;
-unshift @INC, "$dirname/p5-Job";
 if (not require Job::Parallel) { die "Cannot find Job::Parallel" }
 if (not require Job::Timed) { die "Cannot find Job::Timed" }
 
